bb026593624e32bd3ce5bb45bf530bb9bbfb0c2a
Nikola Borisov <nikola.borisov2004@gmail.com>
2026-08-13 03:45:48 +0200

    Dodat alijas za home page rutu: "pocetna"

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'pocetna', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'products',
     loadComponent: () => import('./pages/products/products.component').then((m) => m.ProductsComponent),
